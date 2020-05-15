c35289d010d89b773169bbac10d8d247c2fa6dc5
Nikolai Pribitkov <pribitkov2013@mail.ru>
2020-05-15 17:54:58 +0300

    add images, audio, create first page

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   menu.classList.toggle('active');
 });
+
+const train = document.querySelector('#radio-one');
+const play = document.querySelector('#radio-two');
+
+train.addEventListener('click', () => {
+  const gallery = document.querySelector('.first-page__list');
+  const pictures = gallery.querySelectorAll('.first-page__link');
+  pictures.forEach((element) => {
+    element.classList.remove('red');
+  });
+});
+
+play.addEventListener('click', () => {
+  const gallery = document.querySelector('.first-page__list');
+  const pictures = gallery.querySelectorAll('.first-page__link');
+  pictures.forEach((element) => {
+    element.classList.add('red');
+  });
+});
